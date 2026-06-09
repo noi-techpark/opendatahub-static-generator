95f536b23dcf66ba8d6f23dda47a43864772feea
RudiThoeni <r.thoeni@noi.bz.it>
2026-06-09 17:54:10 +0200

    adding sync on startup

--- opendatahub-static-generator-worker/Program.cs
+++ opendatahub-static-generator-worker/Program.cs
@@ -1,4 +1,6 @@
 using Quartz;
+using Quartz.Impl.Matchers;
+using Quartz.Listener;
 using GeneratorWorker.Jobs;
 using GeneratorWorker.Models;
 using GeneratorWorker.Services;
@@ -16,8 +18,7 @@ var syncConfig = builder.Configuration.GetSection("Sync").Get<SyncConfig>() ?? n
 
 builder.Services.AddQuartz(q =>
 {
-    //Go trough each rules
-    foreach (var rule in syncConfig.Rules)
+    foreach (var (rule, index) in syncConfig.Rules.Select((r, i) => (r, i)))
     {
         var jobKey = new JobKey(rule.Name, "sync");
 
@@ -26,20 +27,38 @@ builder.Services.AddQuartz(q =>
             .UsingJobData("RuleName", rule.Name)
             .StoreDurably());
 
-        q.AddTrigger(opts => opts
-            .ForJob(jobKey)
-            .WithIdentity($"{rule.Name}-trigger", "sync")
-            .WithCronSchedule(rule.CronExpression, x => x.WithMisfireHandlingInstructionFireAndProceed()));
-
-        if (rule.RunOnStartup)
+        // Only the first rule gets direct triggers; the rest fire via job chaining
+        if (index == 0)
+        {
             q.AddTrigger(opts => opts
                 .ForJob(jobKey)
-                .WithIdentity($"{rule.Name}-startup-trigger", "sync")
-                .StartNow());
+                .WithIdentity($"{rule.Name}-trigger", "sync")
+                .WithCronSchedule(rule.CronExpression, x => x.WithMisfireHandlingInstructionFireAndProceed()));
+
+            if (rule.RunOnStartup)
+                q.AddTrigger(opts => opts
+                    .ForJob(jobKey)
+                    .WithIdentity($"{rule.Name}-startup-trigger", "sync")
+                    .StartNow());
+        }
     }
 });
 
 builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
 
 var host = builder.Build();
-host.Run();
+
+// Chain jobs sequentially: when each job completes it fires the next one in order
+if (syncConfig.Rules.Count > 1)
+{
+    var schedulerFactory = host.Services.GetRequiredService<ISchedulerFactory>();
+    var scheduler = await schedulerFactory.GetScheduler();
+    var chainListener = new JobChainingJobListener("sync-chain");
+    for (var i = 0; i < syncConfig.Rules.Count - 1; i++)
+        chainListener.AddJobChainLink(
+            new JobKey(syncConfig.Rules[i].Name, "sync"),
+            new JobKey(syncConfig.Rules[i + 1].Name, "sync"));
+    scheduler.ListenerManager.AddJobListener(chainListener, GroupMatcher<JobKey>.GroupEquals("sync"));
+}
+
+await host.RunAsync();
